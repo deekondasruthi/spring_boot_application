4e5760b06eedc0ba4cc1e08974b74ef2e6e6bd69
deekondasruthi <sruthideekonda2722@gmail.com>
2025-07-25 12:00:55 +0530

    Update UserController.java

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -1,1 +1,17 @@
 
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin("*")
+public class UserController {
+    @Autowired private UserRepository repo;
+
+    @GetMapping
+    public List<User> getUsers() {
+        return repo.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return repo.save(user);
+    }
+}
